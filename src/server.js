6dd03f96e883a2a26155704d85aa962902430d31
solomiiaprsk <solomiia.pruska@gmail.com>
2025-01-21 20:17:09 -0700

    fix2

--- src/server.js
+++ src/server.js
@@ -44,8 +44,8 @@ export const setupServer = () => {
     }
     res.status(200).json({
       status: 200,
-      message: 'Successfully found contact with id {contactId}!',
-      data: { contactId },
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
     });
   });
 
