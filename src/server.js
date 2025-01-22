064e9ae848aa6e1b5b3b44e25d8ec6de236d735c
solomiiaprsk <solomiia.pruska@gmail.com>
2025-01-21 20:09:19 -0700

    fixed

--- src/server.js
+++ src/server.js
@@ -27,9 +27,12 @@ export const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
       data: contacts,
     });
   });
+
   app.get('/contacts/:contactId', async (req, res, next) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
@@ -39,16 +42,16 @@ export const setupServer = () => {
       });
       return;
     }
-
-    // Відповідь, якщо контакт знайдено
     res.status(200).json({
-      data: contact,
+      status: 200,
+      message: 'Successfully found contact with id {contactId}!',
+      data: { contactId },
     });
   });
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
-      message: 'Not found',
+      message: 'Contact not found',
     });
   });
 
