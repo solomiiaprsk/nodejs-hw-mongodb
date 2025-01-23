a99ee77c2945df71613fb6bea4692d60a548c4a9
solomiiaprsk <solomiia.pruska@gmail.com>
2025-01-22 22:20:06 -0700

    fix7

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ export const setupServer = () => {
     });
   });
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
+  app.get('/contacts/:contactId', async (req, res) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
     if (!contact) {
