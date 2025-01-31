285ff3ce1db368e827f9332fb265930e98df0678
solomiiaprsk <solomiia.pruska@gmail.com>
2025-01-30 21:24:05 -0700

    fix

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,7 +7,7 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 
-import { ctrlWrapper } from '../utils/ctrlWrapper';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
