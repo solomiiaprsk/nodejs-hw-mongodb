2074b556681f44fc55dd45ba4b849b514f10a65e
solomiiaprsk <148825342+solomiiaprsk@users.noreply.github.com>
2025-02-24 07:49:31 -0700

    fix2

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -27,7 +27,7 @@ const contactsSchema = new Schema(
       type: Schema.Types.ObjectId,
       required: true,
     },
-    photo: { type: String, default: null },
+    photo: { type: String, required: true, default: null },
   },
   {
     timestamps: true,
