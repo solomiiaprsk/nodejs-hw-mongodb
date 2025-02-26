16d95e537f8cfc872ac77cd71cb3c12dd1e6d617
solomiiaprsk <solomiia.pruska@gmail.com>
2025-02-26 05:48:30 -0700

    port

--- src/server.js
+++ src/server.js
@@ -64,6 +64,9 @@ export const setupServer = () => {
   //   });
   // });
 
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
@@ -73,6 +76,4 @@ export const setupServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
 };
